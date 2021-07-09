91b7009ef4814adb2bea69b02a05900425c87ddc
John Micko <jrmicko5012@gmail.com>
2021-07-09 21:07:46 +0000

    test public api with promises and callbacks

--- server/routes/test.router.js
+++ server/routes/test.router.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const CoinbasePro = require('coinbase-pro');
 
 const router = express.Router();
+const endpoint = 'https://api-public.sandbox.pro.coinbase.com';
+const publicClient = new CoinbasePro.PublicClient(endpoint);
 
 /**
  * GET route template
  */
 router.get('/', (req, res) => {
+  // testing coinbase public api
+  // console.log(
+  //   publicClient.getProducts()
+  // );
+
+  // using callback
+  publicClient.getProducts((error, response, data) => {
+    if (error) {
+      // handle the error
+      console.log(error);
+    } else {
+      // work with data
+      console.log('using callback');
+      console.log(data);
+    }
+  });
+
+  // using promises
+  publicClient
+  .getProducts()
+  .then(data => {
+    // work with data
+    console.log('using promises');
+    console.log(data);
+  })
+  .catch(error => {
+    // handle the error
+    console.log(error);
+  });
+
   // GET route code here
   console.log('in the server test GET route');
   const queryText = `
