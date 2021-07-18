e49a241dcdfaee04b101ff27e1db978c73eaad59
John Micko <jrmicko5012@gmail.com>
2021-07-18 04:49:13 +0000

    add socket.io to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-require('dotenv').config();
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+require('dotenv').config();
 const bodyParser = require('body-parser');
 
 
@@ -17,6 +21,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /* Routes */
 app.use('/api/trade', tradeRouter);
 
+// socket.io
+io.on('connection', (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  console.log('a user connected');
+
+  socket.on('send-message', ({recipients, text}) =>{
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r!== recipient)
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients,
+        sender: id,
+        text
+      })
+    });
+  })
+});
+
 // Serve static files
 app.use(express.static('build'));
 
@@ -24,6 +48,6 @@ app.use(express.static('build'));
 const PORT = process.env.PORT || 5000;
 
 /** Listen * */
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
