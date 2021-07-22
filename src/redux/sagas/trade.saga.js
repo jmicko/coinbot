9008aebdef950cdfa6448926cae4fd501dadf6f2
John Micko <jrmicko5012@gmail.com>
2021-07-22 12:20:52 -0500

    remove unneeded saga functions

--- src/redux/sagas/trade.saga.js
+++ src/redux/sagas/trade.saga.js
@@ -11,7 +11,6 @@ function* toggleBot(action) {
     }
 }
 
-
 function* startTrade(action) {
     try {
         const response = yield axios.post(`/api/trade/order`, action.payload);
@@ -21,40 +20,10 @@ function* startTrade(action) {
     }
 }
 
-function* buybtc(action) {
-    try {
-        const response = yield axios.post('/api/trade/buybtc');
-        console.log('back from server', response.data.rows);
-        console.log('in the buy btc saga function');
-        // now that the session has ended on the server
-        // remove the client-side user object to let
-        // the client-side code know the user is logged out
-        yield put({ type: 'RETURN_TEST_3', payload: 'payload from saga file' });
-    } catch (error) {
-        console.log('Feedback get request failed', error);
-    }
-}
-
-function* sellbtc(action) {
-    try {
-        console.log('in the sell btc saga function');
-        // yield axios.get('/api/test/');
-        const response = yield axios.get('/api/test');
-        console.log('back from server', response);
-
-        // now that the session has ended on the server
-        // remove the client-side user object to let
-        // the client-side code know the user is logged out
-        yield put({ type: 'RETURN_TEST_3', payload: response });
-    } catch (error) {
-        console.log('Feedback get request failed', error);
-    }
-}
-
 function* tradeSaga() {
     yield takeLatest('TOGGLE_BOT', toggleBot);
-    yield takeLatest('BUY_BTC', buybtc);
-    yield takeLatest('SELL_BTC', sellbtc);
+    // yield takeLatest('BUY_BTC', buybtc);
+    // yield takeLatest('SELL_BTC', sellbtc);
     yield takeLatest('START_TRADE', startTrade);
 }
 
