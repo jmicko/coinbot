ef6bef36c1092e6348fd9165fcb405aec1decba4
John Micko <jrmicko5012@gmail.com>
2021-07-27 01:39:24 -0500

    add api URI to env file but default to sandbox

--- server/modules/authedClient.js
+++ server/modules/authedClient.js
@@ -4,15 +4,13 @@ const key = process.env.SANDBOXKEY;
 const secret = process.env.SANDBOXSECRET;
 const passphrase = process.env.SANDBOXPASSWORD;
 
-// const apiURI = 'https://api.pro.coinbase.com';
-const sandboxURI = 'https://api-public.sandbox.pro.coinbase.com';
+const apiURI = process.env.APIURI || 'https://api-public.sandbox.pro.coinbase.com';
 
 const authedClient = new CoinbasePro.AuthenticatedClient(
     key,
     secret,
     passphrase,
-    // apiURI
-    sandboxURI
+    apiURI
   );
 
   module.exports = authedClient;
