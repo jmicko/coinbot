4074f47d70551dc0325546611ee844c1e4dcbd93
John Micko <jrmicko5012@gmail.com>
2021-07-31 15:56:19 -0500

    move logout button, create settings button which does nothing for now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,25 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { useSelector, useDispatch } from 'react-redux';
-import { connect, useDispatch } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import { connect } from 'react-redux';
 import Trade from '../Trade/Trade.js';
 import Updates from '../Updates/Messages.js';
 import ToggleBot from '../ToggleBot/ToggleBot'
 import TradeList from '../TradeList/TradeList'
 import Status from '../Status/Status'
 import './Home.css'
-// import mapStoreToProps from '../../redux/mapStoreToProps';
+
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
 // component.
 function Home(props) {
-
-  const dispatch = useDispatch();
-
-  const logout = () => {
-    console.log('logout button clicked');
-    dispatch({ type: 'LOGOUT' })
-  }
   
   return (
     <div className="Home">
-      <> {JSON.stringify(props.store.accountReducer)} </>
+      {/* <> {JSON.stringify(props.store.accountReducer)} </> */}
       <header className="header">
         <h2>WE USE COINBOT.</h2>
-        <button onClick={logout}>Log Out</button>
+        {/* <button className="btn-logout btn-blue" onClick={logout}>Log Out</button> */}
       </header>
       <ToggleBot />
       <Trade />
@@ -41,4 +31,4 @@ function Home(props) {
   );
 }
 
-export default connect(mapStoreToProps)(Home);
+export default connect()(Home);
