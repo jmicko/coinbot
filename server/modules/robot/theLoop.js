519103f330a2438fc28a1d5b423cb2eed0da9451
John Micko <jrmicko5012@gmail.com>
2021-08-03 16:19:21 -0500

    check if dbOrder before calling coinbase api

--- server/modules/robot/theLoop.js
+++ server/modules/robot/theLoop.js
@@ -3,36 +3,19 @@ const authedClient = require('../authedClient');
 const databaseClient = require('../databaseClient/databaseClient');
 const socketClient = require('../socketClient');
 const robot = require('./robot');
-
-// todo - get rid of these when sure they are not needed anywhere else
-// const orderSubtractor = require('./orderSubtractor');
-// const exchange = require('./exchange');
 const sleep = require('./sleep');
 const flipTrade = require('./flipTrade');
 
 // set up side toggle - can be boolean, may as well be buys first since trade-pairs are always buys first
+// needs to be outside the loop so it can persist 
 let checkingBuys = true;
 
 // This entire function needs to be careful with async coding
 // Needs to wait for confirmation at every step, or it may act on old data and oversell/overbuy.
 const theLoop = async () => {
   let dbOrder;
-
-  /* bot does not need to check all open orders.
-  Only highest priced buy, and lowest priced sell.
-  If either of those has not settled, neither will have the next highest buy/lowest sell.
-  No point in processing them.
-  
-  Can eliminate looping through all open orders entirely by only checking buy/sell at a time. 
-  Make a toggle that gets flipped at the end of the function to check other side.
-  Aka if it just checked the highest buy, switch to sell, then on next loop it will check highest sell.
-  If a trade has settled, the price is likely moving in that direction, so don't flip the switch. 
-  Keep checking that side in case the price moved a lot in that direction. 
-  
-  this also means no need to get all open orders from coinbase which can be taxing if there are a lot,
-  since we only check one at a time. This newer loop means far less data processing, fewer API calls 
-  (only 2), and a much less complicated loop */
-
+  let cbOrder;
+  // stop the bot if it has been toggled off
   if (!robot.looping) {
     return
   }
@@ -49,37 +32,33 @@ const theLoop = async () => {
       [dbOrder] = await databaseClient.getUnsettledTrades('lowSell');
       // console.log('lowSell', trade);
     }
-    console.log('highBuy or lowSell', dbOrder.id);
-    // check order against coinbase
-    let cbOrder = await authedClient.getOrder(dbOrder.id);
-
-    console.log('order from coinbase either high buy or low sell', cbOrder.settled);
-    console.log('order from database either high buy or low sell', dbOrder.settled);
-
-    // flip trade and update if needed...
-    if (cbOrder.settled) {
-      // flip sides on trade
-      const tradeDetails = flipTrade(dbOrder);
-      console.log(tradeDetails);
-      // send new order
-      let pendingTrade = await authedClient.placeOrder(tradeDetails);
-      // store new order in db
-      await databaseClient.storeTrade(pendingTrade, dbOrder);
-      // update old order in db
-      const queryText = `UPDATE "orders" SET "settled" = NOT "settled", "done_at" = $1, "fill_fees" = $2, "filled_size" = $3, "executed_value" = $4 WHERE "id"=$5;`;
-      await pool.query(queryText, [
-        cbOrder.done_at,
-        cbOrder.fill_fees,
-        cbOrder.filled_size,
-        cbOrder.executed_value,
-        cbOrder.id
-      ]);
-      socketClient.emit('update', {
-        message: `an exchange was made`,
-        orderUpdate: true
-      });
+    // if there is an order, check order against coinbase
+    if (dbOrder) {
+      cbOrder = await authedClient.getOrder(dbOrder.id);
+      if (cbOrder.settled) {
+        // flip trade and update if needed...
+        const tradeDetails = flipTrade(dbOrder);
+        console.log(tradeDetails);
+        // send new order
+        let pendingTrade = await authedClient.placeOrder(tradeDetails);
+        // store new order in db
+        await databaseClient.storeTrade(pendingTrade, dbOrder);
+        // update old order in db
+        const queryText = `UPDATE "orders" SET "settled" = NOT "settled", "done_at" = $1, "fill_fees" = $2, "filled_size" = $3, "executed_value" = $4 WHERE "id"=$5;`;
+        await pool.query(queryText, [
+          cbOrder.done_at,
+          cbOrder.fill_fees,
+          cbOrder.filled_size,
+          cbOrder.executed_value,
+          cbOrder.id
+        ]);
+        socketClient.emit('update', {
+          message: `an exchange was made`,
+          orderUpdate: true
+        });
+      }
     } else {
-      // else flip side toggle
+      // ...else flip side toggle
       checkingBuys = !checkingBuys;
     }
   } catch (error) {
@@ -93,18 +72,18 @@ const theLoop = async () => {
       if (error.data.message === 'NotFound') {
         console.log('order not found in account. deleting from db', dbOrder);
         const queryText = `DELETE from "orders" WHERE "id"=$1;`;
-        await pool.query(queryText, [dbOrder.id])
-          .then(() => {
-            console.log('exchange was tossed lmao');
-            socketClient.emit('update', {
-              message: `exchange was tossed out of the ol' databanks`,
-              orderUpdate: true
-            });
-          })
+        await pool.query(queryText, [dbOrder.id]);
+        // .then(() => {
+        console.log('exchange was tossed lmao');
+        socketClient.emit('update', {
+          message: `exchange was tossed out of the ol' databanks`,
+          orderUpdate: true
+        });
+        // })
       }
     } else {
       console.log('yousa got a biiiig big problems', error);
-      socket.emit('message', { message: 'big doo doo' });
+      socketClient.emit('message', { message: 'big doo doo' });
     }
   } finally {
     if (robot.looping) {
