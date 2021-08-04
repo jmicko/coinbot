389fee88b568a16e2544e92eaae68fb480b2ccc3
John Micko <jrmicko5012@gmail.com>
2021-08-04 14:42:56 -0500

    add connection settings buttons and description

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -9,9 +9,17 @@ function Home(props) {
 
     return (
       <div className="Settings">
-        <>{JSON.stringify(props)}</>
-        <p>Some settings or whatever</p>
-        <p>Some settings or whatever</p>
+        {/* <>{JSON.stringify(props)}</> */}
+        <h2 className="settings-header">Settings</h2>
+        <h4>Connection Method</h4>
+        <p>
+          REST is slower but more reliable. Websocket is faster and better for hundreds of open orders
+          placed very close together, but is less reliable on bad
+          internet connections. With Websocket, dropped messages are not recovered, and you should
+          occasionally switch to REST to synchronize.
+        </p>
+        <button className="btn-blue">REST</button>
+        <button className="btn-blue">Websocket</button>
         <p>Some settings or whatever</p>
         <p>Some settings or whatever</p>
         <p>Some settings or whatever</p>
