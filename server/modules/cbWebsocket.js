32ae60a2290ee0c2919edcd00b19659c88288491
John Micko <jrmicko5012@gmail.com>
2021-08-14 18:09:00 -0500

    make function to handle filled orders from ws

--- server/modules/cbWebsocket.js
+++ server/modules/cbWebsocket.js
@@ -14,8 +14,14 @@ const cbWebsocket = new CoinbasePro.WebsocketClient(
 const handleUpdate = (data) => {
   // console.log(data);
   if (data.profile_id && data.type === 'done') {
-    console.log('reason from Coinbase websocket feed:', data.reason);
+    (data.reason === 'filled')
+    ? handleFilled(data)
+    : console.log('reason from Coinbase websocket feed:', data.reason);
   }
+}
+
+const handleFilled = (order) => {
+  console.log('just filled:', order);
 }
 
 module.exports = {
