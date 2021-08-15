275b06458f2383b3f4024f9a48c73acb8f2727e8
John Micko <jrmicko5012@gmail.com>
2021-08-15 10:46:51 -0500

    Highlight single trade on hover. Helps to see which trade is selected when for example deleting a trade based on price. Delete button and price are far away from each other and hard to trace which one is being deleted.

--- src/components/SingleTrade/SingleTrade.js
+++ src/components/SingleTrade/SingleTrade.js
@@ -13,50 +13,53 @@ function SingleTrade(props) {
     //   return
     // }
     // this formula is ridiculous but hey, at least I didn't inject it right into the html, right?
-    const profit = Math.round((((props.order.original_sell_price * props.order.size - props.order.original_buy_price * props.order.size)) - ((props.store.accountReducer.feeReducer.maker_fee_rate * props.order.original_buy_price * props.order.size)+(props.store.accountReducer.feeReducer.maker_fee_rate * props.order.original_sell_price * props.order.size))) * 10000) / 10000;
+    const profit = Math.round((((props.order.original_sell_price * props.order.size - props.order.original_buy_price * props.order.size)) - ((props.store.accountReducer.feeReducer.maker_fee_rate * props.order.original_buy_price * props.order.size) + (props.store.accountReducer.feeReducer.maker_fee_rate * props.order.original_sell_price * props.order.size))) * 10000) / 10000;
     setProfit(profit);
   }, [props.store.accountReducer, props.order.original_sell_price, props.order.original_buy_price, props.order.size]);
 
-  // delete the order if the abandone button is clicked.
+  // delete the order if the abandon button is clicked.
   // the loop already detects deleted orders, so only need to make a call to coinbase
   // no need to bother the database if it is busy
   function deleteOrder() {
     console.log('clicked delete');
     setDeleting(true)
-      dispatch({
-        type: 'DELETE_TRADE', payload: {
-          id: props.order.id,
-        }
-      })
+    dispatch({
+      type: 'DELETE_TRADE', payload: {
+        id: props.order.id,
+      }
+    })
   }
 
   // todo - probably need to refactor this thing asap. Should use more useState hooks to make these strings a bit less horrifying
   // postgres is much better at math using exact
 
   return (
-    <div className={`${props.order.side}`}>
-      {(deleting === true)
-      ? <p className="deleting">Deleting...</p>
-    : <button className="btn-red" onClick={() => {deleteOrder()}}>Abandon</button>
-    }
-      <p className="single-trade">
-        <strong>
-          Price: </strong>
-        {(props.order.side === 'sell')
-          ? Number(props.order.original_sell_price)
-          : Number(props.order.original_buy_price)
-        } ~ <strong>
+    <div className={`Single-trade ${props.order.side}`}>
+      <div className={"overlay"}>
+
+        {(deleting === true)
+          ? <p className="deleting">Deleting...</p>
+          : <button className="btn-red" onClick={() => { deleteOrder() }}>Abandon</button>
+        }
+        <p className="single-trade-text">
+          <strong>
+            Price: </strong>
           {(props.order.side === 'sell')
-            ? 'Bought at:'
-            : 'Sells for:'
-          } </strong>
-        {(props.order.side === 'sell')
-          ? Number(props.order.original_buy_price)
-          : Number(props.order.original_sell_price)
-        } ~ <strong>Size </strong>{Number(props.order.size)} ~
-        <strong>Value</strong> ${Math.round((props.order.price * props.order.size) * 100) / 100} ~
-        <strong> Pair Profitability</strong> ${profit}
-      </p>
+            ? Number(props.order.original_sell_price)
+            : Number(props.order.original_buy_price)
+          } ~ <strong>
+            {(props.order.side === 'sell')
+              ? 'Bought at:'
+              : 'Sells for:'
+            } </strong>
+          {(props.order.side === 'sell')
+            ? Number(props.order.original_buy_price)
+            : Number(props.order.original_sell_price)
+          } ~ <strong>Size </strong>{Number(props.order.size)} ~
+          <strong>Value</strong> ${Math.round((props.order.price * props.order.size) * 100) / 100} ~
+          <strong> Pair Profitability</strong> ${profit}
+        </p>
+      </div>
     </div>
   )
 }
