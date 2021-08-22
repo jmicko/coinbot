9f6787072a36c783cb20fd7ee24a7bc67694576d
John Micko <jrmicko5012@gmail.com>
2021-08-22 11:52:08 -0500

    clean up cbWebsocket handleFilled function now it just pulls the filled order from the db and adds it to the tradeQueue

--- server/modules/cbWebsocket.js
+++ server/modules/cbWebsocket.js
@@ -38,92 +38,22 @@ const handleCanceled = async (canceledOrder) => {
 }
 
 const handleFilled = async (cbOrder, repeats) => {
-  // cbOrder.isNew = false;
-  robot.wsTrading++;
-  // robot.busy shows how many connections have been made to cb. 
-  // stay under 15/s or rate limiting will start returning errors
-  if (robot.busy <= 15) {
-    // add one to busy. This allows pile up of ws handlers. Earlier handlers will not set busy 
-    // to false while later handlers are still busy.
-    // busy will just go down to 0 when not busy
-    try {
-      robot.busy++;
-      // console.log('should be  little more busy?', robot.busy);
-      // console.log('just filled:', cbOrder);
-      // get settled trade from db
-      const dbOrderRows = await databaseClient.getSingleTrade(cbOrder.order_id);
-      if (dbOrderRows[0] && dbOrderRows[0].id) {
-        const dbOrder = dbOrderRows[0];
-        robot.addToTradeQueue(dbOrder);
-        // console.log('database order returns:', dbOrder);
-        // console.log('there is an order');
-        // flip the trade
-        const tradeDetails = robot.flipTrade(dbOrder)
-        // console.log('trade details:', tradeDetails);
-
-        // send the new trade
-        // let pendingTrade = await authedClient.placeOrder(tradeDetails);
-        // console.log('order placed by ws at price:', tradeDetails.price);
-        // store new order in db
-        // await databaseClient.storeTrade(pendingTrade, dbOrder);
-        // update old order in db
-        // unfortunately ws does not return some wanted data 
-        // so we will need to manually get it if we want to see profits etc
-        const queryText = `UPDATE "orders" SET "settled" = NOT "settled", "done_at" = $1 WHERE "id"=$2;`;
-        await pool.query(queryText, [
-          cbOrder.time,
-          // cbOrder.fill_fees, // none
-          // cbOrder.filled_size, // none
-          // cbOrder.executed_value, // not really
-          cbOrder.order_id
-        ]);
-        socketClient.emit('update', {
-          message: `an exchange was made`,
-          orderUpdate: true
-        });
-
-      } else {
-        // when an order is first placed, it takes time to store in db and may return nothing
-        // if that is the case, call this function again
-        // however if a random order is filled that was never in the db, this makes an infinite loop
-        // so only repeat this action 10 times then give up because it def is not there
-        repeats++;
-        console.log('repeats', repeats);
-        // wait a little longer with each try
-        await robot.sleep(repeats * 100)
-        if (repeats < 10) {
-          handleFilled(cbOrder, repeats);
-        }
-      }
-    } catch (error) {
-      if (error.response && error.response.statusCode && error.response.statusCode === 429) {
-        console.log('status code in cbWebsocket', error.response.statusCode);
-        console.log('error data with cb websocket', error.data);
-        console.log('robot busy:', robot.busy);
-        await robot.sleep(10)
-        handleFilled(cbOrder);
-      }
-      if (error.statusMessage) {
-        console.log('error message with cb websocket', error.statusMessage);
-      }
-    } finally {
-
-      // tell that the robot is no longer trading
-      robot.wsTrading--;
-      console.log('-----is ws still trading?', robot.wsTrading);
-      // wait for one second so rate limit stays under 15/s
-      // then subtract one from busy to clear up the connection
-      await robot.sleep(1000);
-      robot.busy--;
-      // console.log('busy?', robot.busy);
-    }
-    // else triggers if there are too many connections to cb.
-    // wait a short time then trigger the function again.
+  const dbOrderRows = await databaseClient.getSingleTrade(cbOrder.order_id);
+  if (dbOrderRows[0] && dbOrderRows[0].id) {
+    const dbOrder = dbOrderRows[0];
+    robot.addToTradeQueue(dbOrder);
   } else {
-    await robot.sleep(100)
-    handleFilled(cbOrder);
-    robot.wsTrading--;
-    console.log('-----is ws still trading?', robot.wsTrading);
+    // when an order is first placed, it takes time to store in db and may return nothing
+    // if that is the case, call this function again
+    // however if a random order is filled that was never in the db, this makes an infinite loop
+    // so only repeat this action 10 times then give up because it def is not there
+    repeats++;
+    console.log('repeats', repeats);
+    // wait a little longer with each try
+    await robot.sleep(repeats * 100)
+    if (repeats < 10) {
+      handleFilled(cbOrder, repeats);
+    }
   }
 }
 
