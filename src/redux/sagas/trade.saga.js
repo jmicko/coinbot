5867c22995a73d9a91e8bda91f4f4a74858ed321
John Micko <jrmicko5012@gmail.com>
2021-10-21 13:31:47 -0500

    remove toggle from trade saga

--- src/redux/sagas/trade.saga.js
+++ src/redux/sagas/trade.saga.js
@@ -1,19 +1,6 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-
-function* toggleBot(action) {
-  try {
-    const response = yield axios.post(`/api/trade/toggle`);
-    console.log('response is.....', response);
-  } catch (error) {
-    console.log('POST toggle route has failed', error)
-    if (error.response.status === 403) {
-      yield put({ type: 'UNSET_USER' });
-    }
-  }
-}
-
 function* startTrade(action) {
   try {
     console.log('payload is:', action.payload);
@@ -41,7 +28,6 @@ function* deleteTrade(action) {
 }
 
 function* tradeSaga() {
-  yield takeLatest('TOGGLE_BOT', toggleBot);
   yield takeLatest('START_TRADE', startTrade);
   yield takeLatest('DELETE_TRADE', deleteTrade);
 }
