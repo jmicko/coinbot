e48ad90b74cfec993cbe9e69e5f7e8ef353857ec
John Micko <jrmicko5012@gmail.com>
2021-11-02 14:59:39 -0500

    delete route marks as canceled and sends to cb. the loop will take care of deleting from db

--- server/routes/trade.router.js
+++ server/routes/trade.router.js
@@ -25,22 +25,22 @@ router.post('/', rejectUnauthenticated, async (req, res) => {
     product_id: order.product_id,
     stp: 'cn',
   };
-try {
-  // send the new order with the trade details
-  let pendingTrade = await authedClient.placeOrder(tradeDetails);
-  // store the new trade in the db. the trade details are also sent to store trade position prices
-  let results = await databaseClient.storeTrade(pendingTrade, tradeDetails);
-  // send OK status
-  res.sendStatus(200);
-} catch (err) {
-  if (err.response.statusCode === 400) {
-    console.log('Insufficient funds!');
-  } else {
-    console.log('problem in sending trade post route', err);
+  try {
+    // send the new order with the trade details
+    let pendingTrade = await authedClient.placeOrder(tradeDetails);
+    // store the new trade in the db. the trade details are also sent to store trade position prices
+    let results = await databaseClient.storeTrade(pendingTrade, tradeDetails);
+    // send OK status
+    res.sendStatus(200);
+  } catch (err) {
+    if (err.response.statusCode === 400) {
+      console.log('Insufficient funds!');
+    } else {
+      console.log('problem in sending trade post route', err);
+    }
+    // send internal error status
+    res.sendStatus(500);
   }
-  // send internal error status
-  res.sendStatus(500);
-}
 });
 
 
@@ -50,49 +50,46 @@ try {
 router.delete('/', rejectUnauthenticated, async (req, res) => {
   // DELETE route code here
   const orderId = req.body.id;
-    console.log('in the server trade DELETE route', req.body.id)
-    authedClient.cancelOrder(orderId)
-      .then(data => {
-        console.log('order was deleted successfully from cb', data);
-        const queryText = `DELETE from "orders" WHERE "id"=$1;`;
-        pool.query(queryText, [data])
-          .then(() => {
-            socketClient.emit('message', {
-              message: `order was tossed out of ol' databanks`,
-              orderUpdate: true
-            });
-            console.log('deleted from db as well');
-            res.sendStatus(200);
-          })
-      })
-      .catch((error) => {
-        if (error.data?.message) {
-          console.log('error message, trade router DELETE:', error.data.message);
-          // orders that have been canceled are deleted from coinbase and return a 404.
-          // error handling should delete them from db and not worry about coinbase since there is no other way to delete
-          // but also send one last delete message to Coinbase just in case it finds it again, but with no error checking
-          if (error.data.message === 'order not found') {
-            console.log('order not found in account. deleting from db', orderId);
-            const queryText = `DELETE from "orders" WHERE "id"=$1;`;
-            pool.query(queryText, [orderId])
-              .then(() => {
-                console.log('exchange was tossed lmao');
-                socketClient.emit('message', {
-                  message: `exchange was tossed out of the ol' databanks`,
-                  orderUpdate: true
-                });
-                res.sendStatus(200)
-              })
-              .catch((error) => {
-                console.log('error in trade.router.js delete route', error);
-                res.sendStatus(500)
-              })
-          }
-        } else {
-          console.log('something failed', error);
-          res.sendStatus(500)
+  console.log('in the server trade DELETE route', req.body.id)
+
+  // mark as canceled in db
+  const queryText = `UPDATE "orders" SET "will_cancel" = true WHERE "id"=$1;`;
+  let result = await pool.query(queryText, [orderId]);
+  // send cancelOrder to cb
+
+
+  authedClient.cancelOrder(orderId)
+    .then(data => {
+      console.log('order was deleted successfully from cb', data);
+    })
+    .catch((error) => {
+      if (error.data?.message) {
+        console.log('error message, trade router DELETE:', error.data.message);
+        // orders that have been canceled are deleted from coinbase and return a 404.
+        // error handling should delete them from db and not worry about coinbase since there is no other way to delete
+        // but also send one last delete message to Coinbase just in case it finds it again, but with no error checking
+        if (error.data.message === 'order not found') {
+          console.log('order not found in account. deleting from db', orderId);
+          const queryText = `DELETE from "orders" WHERE "id"=$1;`;
+          pool.query(queryText, [orderId])
+            .then(() => {
+              console.log('exchange was tossed lmao');
+              socketClient.emit('message', {
+                message: `exchange was tossed out of the ol' databanks`,
+                orderUpdate: true
+              });
+              res.sendStatus(200)
+            })
+            .catch((error) => {
+              console.log('error in trade.router.js delete route', error);
+              res.sendStatus(500)
+            })
         }
-      });
+      } else {
+        console.log('something failed', error);
+        res.sendStatus(500)
+      }
+    });
 });
 
 
