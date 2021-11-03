1753ebea5531f9b5dfec57306f10c8b57d0bbc16
John Micko <jrmicko5012@gmail.com>
2021-11-03 10:20:37 -0500

    Clean up filler content

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -23,18 +23,8 @@ function Settings(props) {
 
     return (
       <div className="Settings">
-        {/* <>{JSON.stringify(props)}</> */}
         <button className="btn-logout btn-red" onClick={() => {props.clickSettings()}}>X</button>
         <h2 className="settings-header">Settings</h2>
-        {/* <h4>Connection Method</h4>
-        <p>
-          REST is slower but more reliable. Websocket is faster and better for hundreds of open orders
-          placed very close together, but is less reliable on bad
-          internet connections. With Websocket, dropped messages are not recovered, and you should
-          occasionally switch to REST to synchronize.
-        </p>
-        <button className="btn-blue">REST</button>
-        <button className="btn-blue">Websocket</button> */}
         <h4>Delete All Trades</h4>
         <p>Danger! This button will delete all your positions! Press it carefully!</p>
         <button className="btn-blue" onClick={() => { deleteAllOrders() }}>Delete All</button>
@@ -44,9 +34,6 @@ function Settings(props) {
           database. It can sometimes fix issues that cause repeated errors, and may take a few minutes to complete
         </p>
         <button className="btn-logout btn-blue" onClick={() => dispatch({ type: 'SYNC_ORDERS' })}>Sync All Trades</button>
-        <p>Some settings or whatever</p>
-        <p>Some settings or whatever</p>
-        <p>Some settings or whatever</p>
       </div>
     );
   } else {
