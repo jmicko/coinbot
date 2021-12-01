ae46c1efe302dc52bfa71ff21a9f8541eb6bbb6f
John Micko <jrmicko5012@gmail.com>
2021-12-01 10:20:21 -0600

    logout users after unauthorized actions

--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -4,18 +4,15 @@ import axios from 'axios';
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchUser() {
   try {
+    // the config includes credentials which
+    // allow the server session to recognize the user
+    // If a user is logged in, this will return their information
+    // from the server session (req.user)
     const config = {
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true,
     };
 
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    // If a user is logged in, this will return their information
-    // from the server session (req.user)
-
-    /* most of this code is from a previous project. Probably don't need user info on client side,
-      but just commenting out for now. todo - delete later if not needed */
     const response = yield axios.get('/api/user', config);
 
     // now that the session has given us a user object
@@ -33,23 +30,15 @@ function* fetchUsers() {
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true,
     };
-
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    // If a user is logged in, this will return their information
-    // from the server session (req.user)
-
-    /* most of this code is from a previous project. Probably don't need user info on client side,
-      but just commenting out for now. todo - delete later if not needed */
     const response = yield axios.get('/api/user/all', config);
 
-    // now that the session has given us a user object
-    // with an id and username set the client-side user object to let
-    // the client-side code know the user is logged in
     console.log('the users are', response);
     yield put({ type: 'SET_ALL_USERS', payload: response.data });
   } catch (error) {
     console.log('User get request failed', error);
+    if (error.response.status === 403) {
+      yield put({ type: 'UNSET_USER' });
+    }
   }
 }
 
@@ -61,18 +50,16 @@ function* deleteUser(action) {
       data: action.payload
     };
     console.log('deleting user');
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    // If a user is logged in, this will return their information
-    // from the server session (req.user)
+
     const response = yield axios.delete('/api/user/', config);
-    // now that the session has given us a user object
-    // with an id and username set the client-side user object to let
-    // the client-side code know the user is logged in
+
     console.log('the delete user response is', response);
     yield put({ type: 'FETCH_USERS'});
   } catch (error) {
     console.log('User get request failed', error);
+    if (error.response.status === 403) {
+      yield put({ type: 'UNSET_USER' });
+    }
   }
 }
 
@@ -84,18 +71,16 @@ function* approveUser(action) {
       data: action.payload
     };
     console.log('approving user');
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    // If a user is logged in, this will return their information
-    // from the server session (req.user)
+
     const response = yield axios.put('/api/user/approve', config);
-    // now that the session has given us a user object
-    // with an id and username set the client-side user object to let
-    // the client-side code know the user is logged in
+
     console.log('the approve user response is', response);
     yield put({ type: 'FETCH_USERS'});
   } catch (error) {
     console.log('User get request failed', error);
+    if (error.response.status === 403) {
+      yield put({ type: 'UNSET_USER' });
+    }
   }
 }
 
