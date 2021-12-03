1ba60597a2ff7393c3cdd1d99fe1d0f019160925
John Micko <jrmicko5012@gmail.com>
2021-12-03 13:03:47 -0600

    Add reinvest to user table
    Will be used in the future to reinvest profit from trade-pair on the buy side

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -159,6 +159,8 @@ router.post('/factoryReset', rejectUnauthenticated, async (req, res) => {
           "approved" boolean DEFAULT false,
           "paused" boolean DEFAULT false,
           "will_delete" boolean DEFAULT false,
+          "reinvest" boolean DEFAULT false,
+          "reinvest_ratio" integer DEFAULT 0,
           "CB_SECRET" VARCHAR (1000),
           "CB_ACCESS_KEY" VARCHAR (1000),
           "CB_ACCESS_PASSPHRASE" VARCHAR (1000),
