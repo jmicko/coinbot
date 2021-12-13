884fbb7ff2cd1b5d5a777dff8ddece67db8b1831
John Micko <jrmicko5012@gmail.com>
2021-12-12 19:35:39 -0600

    create settings route

---
+++ server/routes/settings.router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+const { rejectUnauthenticated, } = require('../modules/authentication-middleware');
+const databaseClient = require('../modules/databaseClient');
+const socketClient = require('../modules/socketClient');
+const robot = require('../modules/robot');
+const coinbaseClient = require('../modules/coinbaseClient');
+
+
+/**
+ * POST route updating bot speed
+ */
+router.post('/loopSpeed', rejectUnauthenticated, async (req, res) => {
+  // POST route code here
+  const user = req.user;
+  if (user.admin) {
+    console.log('loop speed route hit!');
+  } else {
+    console.log('user is not admin!');
+    res.sendStatus(403)
+  }
+});
+
+
+
+module.exports = router;
