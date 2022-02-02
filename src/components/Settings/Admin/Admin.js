89943e6d89af2384734baa552884f7790655ab6f
John Micko <jrmicko5012@gmail.com>
2022-02-02 14:27:37 -0600

    add input to change full sync frequency

--- src/components/Settings/Admin/Admin.js
+++ src/components/Settings/Admin/Admin.js
@@ -8,6 +8,7 @@ import './Admin.css'
 function Admin(props) {
   const dispatch = useDispatch();
   const [loopSpeed, setLoopSpeed] = useState(1);
+  const [fullSync, setFullSync] = useState(10);
 
   const getUsers = useCallback(
     () => {
@@ -34,6 +35,15 @@ function Admin(props) {
     })
   }
 
+  function sendFullSync() {
+    dispatch({
+      type: 'SEND_FULL_SYNC',
+      payload: {
+        loopSpeed: fullSync
+      }
+    })
+  }
+
   function toggleMaintenance() {
     dispatch({
       type: 'TOGGLE_MAINTENANCE',
@@ -45,6 +55,10 @@ function Admin(props) {
 
   function handleLoopSpeedChange(speed) {
     setLoopSpeed(speed);
+  }
+
+  function handleFullSyncChange(speed) {
+    setFullSync(speed);
   }
 
   useEffect(() => {
@@ -60,7 +74,7 @@ function Admin(props) {
   }, [getUsers, getAllSettings]);
 
   return (
-    <div className="Admin">
+    <div className="Admin settings-panel scrollable">
       {/* <center>
         <p>Admin Settings Page</p>
       </center> */}
@@ -100,6 +114,33 @@ function Admin(props) {
       <br />
       <br />
       <button className={`btn-blue btn-reinvest medium ${props.theme}`} onClick={() => { sendLoopSpeed() }}>Save speed</button>
+
+      <div className="divider" />
+
+      {/* SET FULL SYNC FREQUENCY */}
+
+      <h4>Set Full Sync Frequency</h4>
+      <p>
+        This will adjust how often the bot does a full sync. A full sync takes longer and is more CPU intensive,
+        But will check for and delete extra trades etc. A quick sync only checks for recently settled trades.
+      </p>
+      <p>Current frequency: Every {props.store.settingsReducer.allSettingsReducer.loop_speed} loop{props.store.settingsReducer.allSettingsReducer.loop_speed > 1 && 's'}</p>
+      <label htmlFor="fullSync">
+        Set frequency:
+      </label>
+      <input
+        type="number"
+        name="fullSync"
+        value={fullSync}
+        step={1}
+        max={100}
+        min={1}
+        required
+        onChange={(event) => handleFullSyncChange(Number(event.target.value))}
+      />
+      <br />
+      <br />
+      <button className={`btn-blue btn-reinvest medium ${props.theme}`} onClick={() => { sendFullSync() }}>Save speed</button>
 
       <div className="divider" />
 
