b5b86873eeb802db841716bca809a789868b9fee
John Micko <jrmicko5012@gmail.com>
2022-02-07 19:39:22 -0600

    create confirm component

---
+++ src/components/Confirm/Confirm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import './Confirm.css'
+
+function Confirm(props) {
+
+  return (
+    <div className={`Single-trade`}>
+
+    </div>
+  )
+}
+
+export default connect(mapStoreToProps)(Confirm);
