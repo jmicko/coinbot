ca0c3fba13f5351f559012bcf9a7ca33cef0b15a
John Micko <jrmicko5012@gmail.com>
2022-04-29 22:29:38 -0500

    add a tips checkbox to show tips

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -15,22 +15,40 @@ import BulkDelete from './BulkDelete/BulkDelete';
 
 function Settings(props) {
   const [settingsPage, setSettingsPage] = useState('general');
+  const [tips, setTips] = useState(false);
+
+
 
   if (props.showSettings) {
     return (
       <div className={`Settings ${props.theme}`}>
         <button className={`btn-logout btn-red ${props.store.accountReducer.userReducer.theme}`} onClick={() => { props.clickSettings() }}>X</button>
+
+        <p className="info tips">
+          <strong>Show Tips</strong>
+          {/* <br /> */}
+          <input
+            type="checkbox"
+            id="topping"
+            name="topping"
+            value="Paneer"
+            checked={tips}
+            onChange={() => setTips(!tips)}
+          />
+        </p>
+
         <h2 className="settings-header">Settings</h2>
+
         <SettingsNav setSettingsPage={setSettingsPage} settingsPage={settingsPage} />
         {
           {
-            'general': <General theme={props.theme} />,
-            'investment': <Investment theme={props.theme} />,
-            'history': <History theme={props.theme} />,
-            'autoSetup': <AutoSetup theme={props.theme} priceTicker={props.priceTicker} />,
-            'reset': <Reset theme={props.theme} />,
-            'bulkDelete': <BulkDelete theme={props.theme} />,
-            'admin': <Admin theme={props.theme} />
+            'general': <General theme={props.theme} tips={tips} />,
+            'investment': <Investment theme={props.theme} tips={tips} />,
+            'autoSetup': <AutoSetup theme={props.theme} tips={tips} priceTicker={props.priceTicker} />,
+            'bulkDelete': <BulkDelete theme={props.theme} tips={tips} />,
+            'history': <History theme={props.theme} tips={tips} />,
+            'reset': <Reset theme={props.theme} tips={tips} />,
+            'admin': <Admin theme={props.theme} tips={tips} />
           }[settingsPage]
         }
       </div>
