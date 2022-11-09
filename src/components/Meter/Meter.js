d7563064f1807a4c389b953fd6f4bf151bb32fcc
John Micko <jrmicko5012@gmail.com>
2022-11-09 17:27:29 -0600

    clean up unused code and reorganize

--- src/components/Meter/Meter.js
+++ src/components/Meter/Meter.js
@@ -1,43 +1,33 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import SingleTrade from '../SingleTrade/SingleTrade'
-import coinbotFilled from "../../../src/coinbotFilled.png";
-// import coinbotFilledGif from "../../../src/coinbotFilled.gif";
 import './Meter.css'
 
 
 function Meter(props) {
-  const dispatch = useDispatch();
-
-  // these will store mapped arrays as html so they can be used after page loads
-  const [max, setMax] = useState(100);
-  const [min, setMin] = useState(0);
+  // const dispatch = useDispatch();
   const [difference, setDifference] = useState(1);
-  const [current, setCurrent] = useState(0);
-  const [adjustedCurrent, setAdjustedCurrent] = useState(0);
-  const [percentage, setPercentage] = useState(0);
   const [segmentMap, setSegmentMap] = useState();
 
   useEffect(() => {
-    setCurrent(props.current);
     // do not make these calculations unless there are props coming in
     if (props.max > 0 && props.min >= 0) {
 
+      // let difference = (props.max - props.min).toFixed(0);
       setDifference((props.max - props.min).toFixed(0));
       let adjustedCurrent = props.current - props.min;
-      setAdjustedCurrent(adjustedCurrent.toFixed(2));
 
       // this is the important number
       let percentage = ((adjustedCurrent / difference) * 100).toFixed(0);
-      setPercentage(percentage);
 
+      // start an empty array to store the segments of the meter bar
       let segments = []
       for (let i = 0; i < 100; i++) {
         const element = i;
         segments.unshift({ id: i, percentage })
       }
-      // console.log(segments);
+
+      // use the array to map out segment components. They will just be stacked
       setSegmentMap(segments.map(seg => {
         return (
           <Segment key={seg.id} id={seg.id} percentage={seg.percentage} />
@@ -49,6 +39,7 @@ function Meter(props) {
   function Segment(props) {
     return (
       <div
+      // compare the segment to the percentage, and set the background color accordingly
         className={
           props.id < props.percentage
             ? "black-segment segment"
