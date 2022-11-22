aa6b071c2cdbb5df32470bbe5ab5d7928b7859a3
John Micko <jrmicko5012@gmail.com>
2022-11-21 21:13:04 -0600

    remove passphrase from api details

--- src/components/NotActive/NotActive.js
+++ src/components/NotActive/NotActive.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './NotActive.css';
 
 
 function NotActive(props) {
   const dispatch = useDispatch();
+  const user = useSelector((store) => store.accountReducer.userReducer);
 
   const [key, setKey] = useState('');
-  const [passphrase, setPassphrase] = useState('');
   const [secret, setSecret] = useState('');
   const [URI, setURI] = useState('real');
 
@@ -18,7 +18,6 @@ function NotActive(props) {
       type: 'STORE_API',
       payload: {
         key: key,
-        passphrase: passphrase,
         secret: secret,
         URI: URI
       }
@@ -26,14 +25,13 @@ function NotActive(props) {
 
     // clear the form
     // setKey('');
-    // setPassphrase('');
     // setSecret('');
   }
 
   return (
     <div className="NotActive" >
       <div className="scrollable boxed">
-        <h3 className={`title ${props.theme}`}>You are not active!</h3>
+        <h3 className={`title ${user.theme}`}>You are not active!</h3>
         <p>
           You must store your API details from Coinbase Pro before you can trade. <br/>
           - You can create an API key by clicking your name on Coinbase Pro, and clicking <strong>API</strong>. <br/>
@@ -46,7 +44,7 @@ function NotActive(props) {
         <div className="API">
           <div className="divider" />
           <h4>API</h4>
-          <p>Paste your API key, passphrase, and secret from Coinbase here</p>
+          <p>Paste your API key, and secret from Coinbase here</p>
           {/* form for entering api details */}
           <form className="api-form" onSubmit={submitApi} >
             <label htmlFor="key">
@@ -58,16 +56,6 @@ function NotActive(props) {
               value={key}
               required
               onChange={(event) => setKey(event.target.value)}
-            /><br/>
-            <label htmlFor="passphrase">
-              API Passphrase:
-            </label><br/>
-            <input
-              type="password"
-              name="passphrase"
-              value={passphrase}
-              required
-              onChange={(event) => setPassphrase(event.target.value)}
             /><br/>
             <label htmlFor="secret">
               API Secret:
@@ -83,13 +71,13 @@ function NotActive(props) {
               Real money or sandbox?
             </label><br/>
             {(URI === "real")
-              ? <button className={`btn-green btn-sandbox-api medium ${props.theme}`} onClick={(event) => { event.preventDefault(); setURI("sandbox") }}>Real Money API</button>
-              : <button className={`btn-green btn-sandbox-api medium ${props.theme}`} onClick={(event) => { event.preventDefault(); setURI("real") }}>Sandbox API</button>
+              ? <button className={`btn-green btn-sandbox-api medium ${user.theme}`} onClick={(event) => { event.preventDefault(); setURI("sandbox") }}>Real Money API</button>
+              : <button className={`btn-green btn-sandbox-api medium ${user.theme}`} onClick={(event) => { event.preventDefault(); setURI("real") }}>Sandbox API</button>
             }
             (click to change)
             <br />
             <br />
-            <input className={`btn-store-api btn-blue medium ${props.theme}`} type="submit" name="submit" value="Store API details" />
+            <input className={`btn-store-api btn-blue medium ${user.theme}`} type="submit" name="submit" value="Store API details" />
           </form>
         </div>
       </div>
