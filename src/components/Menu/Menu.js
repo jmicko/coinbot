8cb75db0f17340b942c96e7c6f0607be01d8df88
John Micko <jrmicko5012@gmail.com>
2023-01-05 08:42:50 -0600

    make the menu fit better on smaller screens

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -19,24 +19,26 @@ function Menu(props) {
 
     <div className={`Menu dark ${user.theme}`}>
       <center>
-        <p className="greeting">Hello {user.username}!</p>
+        <p className="greeting">{user.username}
+        &nbsp;
+        <select
+          onChange={(event) => props.setProduct(event.target.value)}
+          value={props.product}
+          >
+          {/* each active product in products should be listed as an option with the value equal to the product_id */}
+          {products?.activeProducts?.map((product, i) => {
+            return (
+              <option key={i} value={product.product_id}>{product.product_id}</option>
+              )
+            })}
+        </select>
+            </p>
       </center>
       {/* {JSON.stringify(products.activeProducts)} */}
       <div className="menu-buttons">
         {/* only show Test button in dev mode */}
         {process.env.NODE_ENV === 'development' && user.admin && <button className={`btn-blue btn-logout ${user.theme}`} onClick={() => dispatch({ type: 'TEST' })}>Test</button>}
         {/* add dropdown selector */}
-        <select
-          onChange={(event) => props.setProduct(event.target.value)}
-          value={props.product}
-        >
-          {/* each active product in products should be listed as an option with the value equal to the product_id */}
-          {products?.activeProducts?.map((product, i) => {
-            return (
-              <option key={i} value={product.product_id}>{product.product_id}</option>
-            )
-          })}
-        </select>
         <button className={`btn-blue btn-logout ${user.theme}`} onClick={() => { props.clickSettings() }}>Settings</button>
         <button className={`btn-blue btn-logout ${user.theme}`} onClick={() => dispatch({ type: 'LOGOUT' })}>Log Out</button>
       </div>
