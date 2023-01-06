b5953921abb246649d26aa57581a82dc44725f9b
John Micko <jrmicko5012@gmail.com>
2023-01-06 15:56:03 -0600

    Add mouse tooltip to graph

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -4,27 +4,49 @@ function Graph(props) {
   const canvasRef = useRef(null);
   const [scaleToZeroX, setScaleToZeroX] = useState(false);
   const [scaleToZeroY, setScaleToZeroY] = useState(false);
-  // console.log('graph props', props.setupResults.options.startingValue);
-  // console.log('graph props', props.setupResults.options.endingValue);
-  // console.log('min size', props.setupResults.options.base_size);
-  // console.log('max size', props.setupResults.options.maxSize);
-  // console.log('setup options', props.setupResults.options);
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
   const setupOptions = props.setupResults.options;
 
   const setupMinSize = props.setupResults.options.base_size;
   const setupMaxSize = props.setupResults.options.maxSize;
 
-  const startingValueX = scaleToZeroX
-    ? 0
-    : props.setupResults.options.startingValue;
-  const endingValue = props.setupResults.options.endingValue;
+  const whichSize = setupOptions.sizeType === 'base' ? 'base_size' : 'buy_quote_size';
+
+  // capture the mouse position
+  const handleMouseMove = (e) => {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setMouse({ x, y });
+  };
+
+  // get the data
+  const data = [].concat.apply([], props.data.map(d => d.props.order));
+
 
   // draw a graph using the canvas element
   useEffect(() => {
+    const startingValueX = scaleToZeroX ? 0 : props.setupResults.options.startingValue;
+    const endingValue = props.setupResults.options.endingValue;
+
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    // const quoteIncrement = Number(props.product.product.quote_increment)
+
+
+    // get the min and max values
+    const minPrice = startingValueX;
+    const maxPrice = endingValue;
+
+    const minSize = scaleToZeroY ? 0 : setupMinSize;
+    const maxSize = setupMaxSize;
+
+    // ensure that there will not be infinity
+    if (minPrice === maxPrice || minSize === maxSize) {
+      return;
+    }
+
 
     // clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -33,30 +55,8 @@ function Graph(props) {
     ctx.fillStyle = 'rgba(230, 255, 255, 1)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // get the data and plot the points
-    const data = [].concat.apply([], props.data.map(d => d.props.order));
-
     // check if the data array is empty or contains only null or undefined values
     if (data.length === 0 || data.every(d => !d)) {
-      return;
-    }
-
-    // get the min and max values
-    // const minPrice = Math.min(...data.map(d => d.original_buy_price));
-    // const maxPrice = Math.max(...data.map(d => d.original_buy_price));
-    const minPrice = startingValueX;
-    const maxPrice = endingValue;
-
-    const minSize = scaleToZeroY
-      ? 0
-      // : Math.min(...data.map(d => d.buy_quote_size));
-      : setupMinSize;
-
-    // const maxSize = Math.max(...data.map(d => d.buy_quote_size));
-    const maxSize = setupMaxSize;
-
-    // ensure that there will not be infinity
-    if (minPrice === maxPrice || minSize === maxSize) {
       return;
     }
 
@@ -64,6 +64,8 @@ function Graph(props) {
     // the graph will be drawn inside this rectangle
     const paddingX = canvas.width * 0.1;
     const paddingY = canvas.height * 0.13;
+    const graphBottom = canvas.height - paddingY
+    const graphRight = canvas.width - paddingX
     const graphWidth = canvas.width - 2 * paddingX;
     const graphHeight = canvas.height - 2 * paddingY;
 
@@ -74,14 +76,14 @@ function Graph(props) {
     // draw the x axis in black
     ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
     ctx.beginPath();
-    ctx.moveTo(paddingX, canvas.height - paddingY);
-    ctx.lineTo(canvas.width - paddingX, canvas.height - paddingY);
+    ctx.moveTo(paddingX, graphBottom);
+    ctx.lineTo(graphRight, graphBottom);
     ctx.stroke();
 
     // draw the y axis in black
     ctx.beginPath();
     ctx.moveTo(paddingX, paddingY);
-    ctx.lineTo(paddingX, canvas.height - paddingY);
+    ctx.lineTo(paddingX, graphBottom);
     ctx.stroke();
 
     // draw the x axis label below the x axis, and below the tick labels
@@ -89,8 +91,7 @@ function Graph(props) {
     ctx.textAlign = 'center';
     ctx.textBaseline = 'top';
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText('Price', canvas.width / 2, canvas.height - paddingY + 25);
-
+    ctx.fillText('Price', canvas.width / 2, graphBottom + 25);
 
     // draw the y axis labels to the left of the y axis
     ctx.save();
@@ -109,25 +110,24 @@ function Graph(props) {
       // draw the tick
       ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
       ctx.beginPath();
-      ctx.moveTo(paddingX + i * xTickInterval, canvas.height - paddingY);
-      ctx.lineTo(paddingX + i * xTickInterval, canvas.height - paddingY + 5);
+      ctx.moveTo(paddingX + i * xTickInterval, graphBottom);
+      ctx.lineTo(paddingX + i * xTickInterval, graphBottom + 5);
       ctx.stroke();
 
       // draw the tick label
       ctx.textAlign = 'center';
       ctx.textBaseline = 'top';
-      ctx.fillText((minPrice + i * (maxPrice - minPrice) / (numXTicks - 1)).toFixed(2), paddingX + i * xTickInterval, canvas.height - paddingY + 10);
+      ctx.fillText((minPrice + i * (maxPrice - minPrice) / (numXTicks - 1)).toFixed(2), paddingX + i * xTickInterval, graphBottom + 10);
 
-
+      if (i === 0) {
+        continue;
+      }
       // draw a light gray graph line
       ctx.strokeStyle = 'rgba(0, 0, 0, .1)';
       ctx.beginPath();
       ctx.moveTo(paddingX + i * xTickInterval, paddingY);
-      ctx.lineTo(paddingX + i * xTickInterval, canvas.height - paddingY);
+      ctx.lineTo(paddingX + i * xTickInterval, graphBottom);
       ctx.stroke();
-
-
-
     }
 
     // draw the y axis ticks
@@ -138,13 +138,13 @@ function Graph(props) {
       // draw the tick
       ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
       ctx.beginPath();
-      ctx.moveTo(paddingX, canvas.height - paddingY - i * yTickInterval);
-      ctx.lineTo(paddingX - 5, canvas.height - paddingY - i * yTickInterval);
+      ctx.moveTo(paddingX, graphBottom - i * yTickInterval);
+      ctx.lineTo(paddingX - 5, graphBottom - i * yTickInterval);
       ctx.stroke();
 
       // draw the tick label sideways and shifted up
       ctx.save();
-      ctx.translate(paddingX - 10, canvas.height - paddingY - i * yTickInterval);
+      ctx.translate(paddingX - 10, graphBottom - i * yTickInterval);
       ctx.rotate(-Math.PI / 2);
       // shift the text up by 10 pixels
       ctx.translate(10, 0);
@@ -153,16 +153,19 @@ function Graph(props) {
       ctx.fillText((minSize + i * (maxSize - minSize) / (numYTicks - 1)).toFixed(2), 0, 0);
       ctx.restore();
 
+      if (i === 0) {
+        continue;
+      }
       // draw a light gray graph line
       ctx.strokeStyle = 'rgba(0, 0, 0, .1)';
       ctx.beginPath();
-      ctx.moveTo(paddingX, canvas.height - paddingY - i * yTickInterval);
-      ctx.lineTo(canvas.width - paddingX, canvas.height - paddingY - i * yTickInterval);
+      ctx.moveTo(paddingX, graphBottom - i * yTickInterval);
+      ctx.lineTo(graphRight, graphBottom - i * yTickInterval);
       ctx.stroke();
-
-
     }
 
+    // save the x values of the data points so the mouse can be used to find the closest data point
+    const xValues = [];
 
     // draw a red point for each data point
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -184,14 +187,14 @@ function Graph(props) {
       //   trade_pair_ratio: 1.1,
       //   userID: 1,
       // }
-      
+
       // map the original buy price to the x coordinate
       // map the base_size to the y coordinate if setupOptions.sizeType is 'base'
       // map the buy_quote_size to the y coordinate if setupOptions.sizeType is 'quote'
-      const whichSize = setupOptions.sizeType === 'base' ? 'base_size' : 'buy_quote_size';
+
       const x = paddingX + (d.original_buy_price - minPrice) / (maxPrice - minPrice) * graphWidth;
-      const y = canvas.height - paddingY - (d[whichSize] - minSize) / (maxSize - minSize) * graphHeight;
-      
+      const y = graphBottom - (d[whichSize] - minSize) / (maxSize - minSize) * graphHeight;
+
       // draw a thin black curved line to the next point
       if (data.indexOf(d) < data.length - 1) {
         ctx.save();
@@ -199,7 +202,7 @@ function Graph(props) {
         ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
         const nextD = data[data.indexOf(d) + 1];
         const nextX = paddingX + (nextD.original_buy_price - minPrice) / (maxPrice - minPrice) * graphWidth;
-        const nextY = canvas.height - paddingY - (nextD[whichSize] - minSize) / (maxSize - minSize) * graphHeight;
+        const nextY = graphBottom - (nextD[whichSize] - minSize) / (maxSize - minSize) * graphHeight;
         ctx.beginPath();
         ctx.moveTo(x, y);
         ctx.bezierCurveTo(x, y, nextX, nextY, nextX, nextY);
@@ -207,38 +210,87 @@ function Graph(props) {
         ctx.restore();
       }
 
-      // draw the red point
+      // draw the point
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       ctx.beginPath();
       ctx.arc(x, y, 1.5, 0, 2 * Math.PI);
       ctx.fill();
 
-
+      // save the x value of the data point
+      xValues.push(x);
 
     });
 
 
+    // draw a vertical line through whichever point is closest to the mouse based on the x values array if the mouse is over the graph
+    if (mouse.x > paddingX && mouse.x < graphRight && mouse.y > paddingY && mouse.y < graphBottom) {
+      // find the closest point to the mouse
+      let closestX = xValues[0];
+      xValues.forEach(x => {
+        if (Math.abs(x - mouse.x) < Math.abs(closestX - mouse.x)) {
+          closestX = x;
+        }
+      });
+      // find the data point that corresponds to the closest x value
+      const closestPoint = data[xValues.indexOf(closestX)];
+
+      // draw the vertical line
+      ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+      ctx.beginPath();
+      const x = paddingX + (closestPoint.original_buy_price - minPrice) / (maxPrice - minPrice) * graphWidth;
+      ctx.moveTo(x, graphBottom);
+      ctx.lineTo(x, paddingY);
+      ctx.stroke();
+
+      // draw the tooltip with the price and size data. not other data is shown because it would be too crowded
+      // if the mouse is too close to the right edge of the canvas, draw the tooltip to the left of the mouse
+      const tooltipX = mouse.x > canvas.width - 100 ? mouse.x - 100 : mouse.x;
+      // if the mouse is too close to the top edge of the canvas, draw the tooltip below the mouse
+      const tooltipY = mouse.y < 30 ? mouse.y + 20 : mouse.y - 40;
+      ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+      ctx.fillRect(tooltipX, tooltipY, 100, 40);
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+      ctx.textAlign = 'left';
+      ctx.textBaseline = 'top';
+      ctx.fillText(`$${closestPoint.original_buy_price.toFixed(2)}`, tooltipX + 5, tooltipY + 5);
+      ctx.fillText(`${Number(closestPoint[whichSize]).toFixed(2)}`, tooltipX + 5, tooltipY + 20);
+      
+
+    }
 
 
+    // add the mouse move listener
+    canvas.addEventListener('mousemove', handleMouseMove);
+
+    // // remove the mousemove event listener when the mouse leaves the canvas
+    // canvas.addEventListener('mouseleave', () => {
+    //   canvas.removeEventListener('mousemove', handleMouseMove);
+    //   setMouse({});
+    // });
+
+    // remove all event listeners when the component unmounts if they haven't been removed already
+    return () => {
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      // canvas.removeEventListener('mouseleave', () => {
+      //   canvas.removeEventListener('mousemove', handleMouseMove);
+      //   setMouse({});
+      // });
+    }
 
 
-
-
-
-    // the component should rerender when the data changes
-  }, [props.data, scaleToZeroX, scaleToZeroY, props.setupResults.options.startingValue, props.setupResults.options.endingValue]);
+  }, [data, canvasRef, mouse, scaleToZeroX, scaleToZeroY]);
 
   return (
     <>
-      {/* {JSON.stringify(props.product)} */}
       <h4>{props.title || "Graph"}</h4>
+      {/* {JSON.stringify(mouse)} */}
+      <br />
       {/* draw the canvas */}
       <canvas ref={canvasRef}
         width={400} height={300}
       />
       <br />
-      {/* {JSON.stringify(scaleToZeroX)} */}
-      {/* checkbox for scale to zero */}
+      {/* checkboxes for scale to zero */}
       <label>
         <input type="checkbox" checked={scaleToZeroX} onChange={e => setScaleToZeroX(e.target.checked)} />
         Start Price at Zero
