91f72c29596b2831b0ba64f5ab43cc5e3612e9e8
John Micko <jrmicko5012@gmail.com>
2023-01-12 07:51:16 -0600

    remove trade saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,5 +1,5 @@
 import { all } from 'redux-saga/effects';
-import tradeSaga from './trade.saga';
+
 import accountSaga from './account.saga';
 import settingsSaga from './settings.saga';
 
@@ -12,7 +12,7 @@ import settingsSaga from './settings.saga';
 // and login triggers setting the user
 export default function* rootSaga() {
   yield all([
-      tradeSaga(),
+
       accountSaga(),
       settingsSaga(),
   ]);
