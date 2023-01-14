c3e5e84913e526df526ef19ce0c1b8f417f02445
John Micko <jrmicko5012@gmail.com>
2023-01-14 11:50:49 -0600

    theme the settings nav

--- src/components/Settings/SettingsNav/SettingsNav.js
+++ src/components/Settings/SettingsNav/SettingsNav.js
@@ -4,19 +4,19 @@ import './SettingsNav.css'
 
 
 function SettingsNav(props) {
-  const { user } = useUser();
+  const { user, theme } = useUser();
 
   return (
     <div className="SettingsNav">
       <center>
-        <button className={`btn-nav ${props.settingsPage === "general" && "selected"}`} onClick={() => { props.setSettingsPage('general') }}>General</button>
-        <button className={`btn-nav ${props.settingsPage === "products" && "selected"}`} onClick={() => { props.setSettingsPage('products') }}>Products</button>
-        <button className={`btn-nav ${props.settingsPage === "investment" && "selected"}`} onClick={() => { props.setSettingsPage('investment') }}>Investment</button>
-        <button className={`btn-nav ${props.settingsPage === "autoSetup" && "selected"}`} onClick={() => { props.setSettingsPage('autoSetup') }}>Auto Setup</button>
-        <button className={`btn-nav ${props.settingsPage === "bulkDelete" && "selected"}`} onClick={() => { props.setSettingsPage('bulkDelete') }}>Bulk Delete</button>
-        <button className={`btn-nav ${props.settingsPage === "history" && "selected"}`} onClick={() => { props.setSettingsPage('history') }}>History</button>
-        <button className={`btn-nav ${props.settingsPage === "reset" && "selected"}`} onClick={() => { props.setSettingsPage('reset') }}>Reset</button>
-        {user.admin && <button className={`btn-nav ${props.settingsPage === "admin" && "selected"}`} onClick={() => { props.setSettingsPage('admin') }}>Admin</button>}
+        <button className={`btn-nav ${theme} ${props.settingsPage === "general" && "selected"}`} onClick={() => { props.setSettingsPage('general') }}>General</button>
+        <button className={`btn-nav ${theme} ${props.settingsPage === "products" && "selected"}`} onClick={() => { props.setSettingsPage('products') }}>Products</button>
+        <button className={`btn-nav ${theme} ${props.settingsPage === "investment" && "selected"}`} onClick={() => { props.setSettingsPage('investment') }}>Investment</button>
+        <button className={`btn-nav ${theme} ${props.settingsPage === "autoSetup" && "selected"}`} onClick={() => { props.setSettingsPage('autoSetup') }}>Auto Setup</button>
+        <button className={`btn-nav ${theme} ${props.settingsPage === "bulkDelete" && "selected"}`} onClick={() => { props.setSettingsPage('bulkDelete') }}>Bulk Delete</button>
+        <button className={`btn-nav ${theme} ${props.settingsPage === "history" && "selected"}`} onClick={() => { props.setSettingsPage('history') }}>History</button>
+        <button className={`btn-nav ${theme} ${props.settingsPage === "reset" && "selected"}`} onClick={() => { props.setSettingsPage('reset') }}>Reset</button>
+        {user.admin && <button className={`btn-nav ${theme} ${props.settingsPage === "admin" && "selected"}`} onClick={() => { props.setSettingsPage('admin') }}>Admin</button>}
       </center>
     </div>
   );
