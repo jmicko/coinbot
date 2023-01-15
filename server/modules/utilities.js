822679e00d6fb942346a7f94353c0c56be19404a
John Micko <jrmicko5012@gmail.com>
2023-01-15 16:17:15 -0600

    improve logging output

--- server/modules/utilities.js
+++ server/modules/utilities.js
@@ -5,13 +5,27 @@ function devLog(...args) {
   if (process.env.NODE_ENV === 'development') {
     console.log(...args);
   }
-
+// console.log('devLog', args);
   // write to a log file withe a timestamp
-  fs.appendFile('./server/logs/log.log', `${new Date().toISOString()} ${args.join(' ')}\n`, (err) => {
-    if (err) {
-      console.log('error logging to file', err);
+  
+  // if any of the args are objects, stringify them
+  args = args.map((arg) => {
+    if (typeof arg === 'object') {
+      return JSON.stringify(arg);
     }
+    return arg;
   });
+
+  
+  fs.appendFile('./server/logs/log.log',
+  
+    `${new Date().toISOString()} ${args.join(' ')}\n`,
+
+    (err) => {
+      if (err) {
+        console.log('error logging to file', err);
+      }
+    });
 
 
 }
