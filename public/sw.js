fcfdf52497c90beca8178a2aeed29387228eb923
John Micko <jrmicko5012@gmail.com>
2023-03-10 11:34:17 -0600

    clean up

--- public/sw.js
+++ public/sw.js
@@ -66,45 +66,6 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
-// self.addEventListener('fetch', (event) => {
-
-//   if (event.request.method !== "GET") return;
-//   // Skip caching for /api requests
-//   if (event.request.url.includes('/api')) {
-//     return;
-//   }
-
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then((response) => {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-
-//         // Clone the request
-//         const fetchRequest = event.request.clone();
-
-//         return fetch(fetchRequest)
-//           .then((response) => {
-//             // Check if we received a valid response
-//             if (!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
-
-//             // Clone the response
-//             const responseToCache = response.clone();
-
-//             caches.open(CACHE_NAME)
-//               .then((cache) => {
-//                 cache.put(event.request, responseToCache);
-//               });
-
-//             return response;
-//           });
-//       })
-//   );
-// });
 
 // handle notification click event
 self.addEventListener('notificationclick', function (event) {
