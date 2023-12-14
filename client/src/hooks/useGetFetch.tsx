cb31a2c0822c7be1fdb8f5a244ff12b39a2c9767
John Micko <jmicko@protonmail.com>
2023-12-14 15:25:00 -0600

    Fix useGetFetch hook to handle errors and add clear function

--- client/src/hooks/useGetFetch.tsx
+++ client/src/hooks/useGetFetch.tsx
@@ -6,18 +6,23 @@ type FetchDataOptions<T> = {
 
 const useGetFetch = <T,>(url: string, options: FetchDataOptions<T>) => {
   const [data, setData] = useState<T>(options.defaultState);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
   const fetchData = async () => {
+    console.log('fetching data');
+
     setIsLoading(true);
     try {
       const response = await fetch(url);
+
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
       }
       const data: T = await response.json();
+      console.log(data, 'response after fetching data');
       setData(data);
+      setError(null);
     } catch (exception) {
       if (exception instanceof Error) {
         setError(exception);
@@ -29,11 +34,23 @@ const useGetFetch = <T,>(url: string, options: FetchDataOptions<T>) => {
     }
   };
 
+  const clear = () => {
+    setData(options.defaultState)
+    setIsLoading(false)
+    setError(null)
+  }
+
   useEffect(() => {
     fetchData();
   }, [url]);
 
-  return { data, isLoading, error, refresh: fetchData };
+  return {
+    data,
+    isLoading,
+    error,
+    refresh: fetchData,
+    clear,
+  };
 };
 
 export default useGetFetch;
